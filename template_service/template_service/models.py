e1fb507561bafd9c920c3a00aa4cf6a66ec7feec
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-11-11 13:34:11 +0100

    feat: template model to store templates

--- template_service/template_service/models.py
+++ template_service/template_service/models.py
@@ -1,3 +1,57 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class TemplateCategory(models.TextChoices):
+    email = "email", "Email"
+    push = "push", "Push Notification"
+
+
+class Template(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    category = models.CharField(
+        max_length=20,
+        choices=TemplateCategory.choices,
+        default=TemplateCategory.email,
+    )
+    subject = models.CharField(max_length=255, blank=True, null=True)
+    body = models.TextField()
+    language = models.CharField(max_length=10, default="en", db_index=True)
+    version = models.IntegerField(default=1)
+
+    context = models.JSONField(default=list, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "templates"
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["name", "category", "language", "-version"]),
+            models.Index(fields=["is_active", "is_deleted"]),
+        ]
+        # makes sure these fields are unique together or no??
+        unique_together = [["name", "category", "language", "version"]]
+
+    def __str__(self):
+        return f"{self.name} ({self.category}) - [lang: {self.language}]"
+
+    def get_latest_version(self):
+        return (
+            Template.objects.filter(
+                name=self.name,
+                category=self.category,
+                language=self.language,
+                is_deleted=False,
+            )
+            .order_by("-version")
+            .first()
+        )
