31f9b9cd53950c2d4eabef0ba7bb22ea1f5b4bfa
fowosere ademola <fowosereademola@gmail.com>
2025-11-13 18:57:21 +0100

    chore: port adding for production

--- email_service/src/main.ts
+++ email_service/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`HTTP server running on port ${process.env.PORT || 3000}`);
 
   const rmqUrl = process.env.RABBITMQ_URL;
